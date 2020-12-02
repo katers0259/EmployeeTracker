47b633fb760e19c078d74b3ffec054a8fcf33abe
Kaitlyn Branagan <kaitlynwebb_46@hotmail.com>
2020-12-01 21:14:34 -0700

    installing the required packages.

--- EmployeeSchema.sql
+++ EmployeeSchema.sql
@@ -4,23 +4,23 @@ CREATE DATABASE employee_DB;
 USE employee_DB;
 
 CREATE TABLE department(
-    id INT PRIMARY KEY,
+    id INT(11) PRIMARY KEY,
     name VARCHAR(30)
 )
 
-CREATE TABLE role(
-    id INT PRIMARY KEY,
+CREATE TABLE employeerole(
+    id INT(11) PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL(10,4),
-    department_id INT
+    department_id INT(30)
 )
 
 CREATE TABLE employee(
-    id INT PRIMARY KEY,
+    id INT(11) PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT
+    role_id INT(30),
+    manager_id INT(30)
 )
 
 SELECT * FROM employee_DB
